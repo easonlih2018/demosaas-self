8adca64006e11a162f30868d06ec7eaf6ab160fa
eason <eason@canway.net>
2019-04-06 23:08:51 +0800

    数据库名称

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -11,7 +11,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': "demosaas-1",                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'root',                        # 你的数据库password
         'HOST': '192.168.200.18',                   		   # 数据库HOST
